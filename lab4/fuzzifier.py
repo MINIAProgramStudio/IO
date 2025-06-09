6f2f64a53c1da8468ec971d80e02560b885413f0
Mykhailo Holub <mishataube2005@gmail.com>
2025-06-09 14:39:02 +0300

    1

--- lab4/fuzzifier.py
+++ lab4/fuzzifier.py
@@ -20,13 +20,13 @@ def soil_wetness(x):
     very_dry = trapmf(x, -0.1, 0, 0.1, 0.2)
     wet = trapmf(x, 0.6, 0.7, 0.8, 0.9)
     very_wet = trapmf(x, 0.8, 0.9, 1, 1.1)
-    return [very_dry, dry, normal, wet, very_wet]
+    return np.array([very_dry, dry, normal, wet, very_wet])
 
 def tank_fullness(x):
     tank_empty = trapmf(x, -0.1, 0, 0, 0.2)
     tank_normal = trapmf(x, 0, 0.2, 0.8, 1)
     tank_overflowing = trapmf(x, 0.9, 1, 1, 1.1)
-    return [tank_empty, tank_normal, tank_overflowing]
+    return np.array([tank_empty, tank_normal, tank_overflowing])
 
 def days_since_rainfall(x):
     rainfall_was_recent = gaussmf(x, 3, 0)
@@ -34,7 +34,7 @@ def days_since_rainfall(x):
 
 def days_since_too_much_water(x):
     do_not_water = gaussmf(x, 1.5, 0)
-    return [do_not_water]
+    return np.array([do_not_water])
 """
 space = np.linspace(0, 1, 101)
 very_dry, dry, normal, wet, very_wet = soil_wetness(space)
@@ -65,8 +65,8 @@ days = np.linspace(0, 10, 11)
 plt.title("Ф. пр. залежні від кількості днів")
 plt.xlabel("Днів після події")
 plt.ylabel("Знач. функ. приналежності")
-rainfall_was_recent = days_since_rainfall(days)
-do_not_water = days_since_too_much_water(days)
+rainfall_was_recent = days_since_rainfall(days)[0]
+do_not_water = days_since_too_much_water(days)[0]
 plt.plot(days, rainfall_was_recent, label = "Нещодавно був дощ")
 plt.plot(days, do_not_water, label = "Нещодавно рослина отримала забагато води")
 plt.legend()
--- lab4/inference_engine.py
+++ lab4/inference_engine.py
@@ -1,13 +1,21 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import fuzzifier
 
 def general_watering_rule(plant_index, garden):
-    plant_soil_wetness_set = fuzzifier.soil_wetness(garden.soil_wetness[plant_index])
-    return [min(max(sum(plant_soil_wetness_set[0:2]), 0), 1)]
+    return general_watering_rule_direct(garden.soil_wetness[plant_index])
+
+def general_watering_rule_direct(soil_wetness):
+    plant_soil_wetness_set = fuzzifier.soil_wetness(soil_wetness)
+    value = np.sum(plant_soil_wetness_set[0:2], axis = 0)
+    return np.array([np.clip(value, 0, 1)])
 
 def general_do_not_water_rule(plant_index, garden):
-    do_not_water_set = fuzzifier.days_since_too_much_water(garden.plants[plant_index].days_since_overwatering)
-    return [do_not_water_set]
+    return general_do_not_water_rule_direct(garden.plants[plant_index].days_since_overwatering)
+
+def general_do_not_water_rule_direct(days_since_overwatering):
+    do_not_water_set = fuzzifier.days_since_too_much_water(days_since_overwatering)[0]
+    return np.array([do_not_water_set])
 
 # twarc -- tap water and rain collector
 
@@ -16,13 +24,72 @@ def twarc_rule(tank_level):
     use_tap_water = tank_level_set[0]
     use_rain_water = 1-use_tap_water
     empty_tank_to_the_most_dry_plant = tank_level_set[2]
-    return [use_tap_water, use_rain_water, empty_tank_to_the_most_dry_plant]
+    return np.array([use_tap_water, use_rain_water, empty_tank_to_the_most_dry_plant])
 
 def desert_water_usage_rule(tank_level, garden):
-    tank_level_set = fuzzifier.tank_fullness(tank_level)
-    rainfall_recency = fuzzifier.days_since_rainfall(garden.days_since_rain)[0]
+    return desert_water_usage_rule_direct(tank_level, garden.days_since_rain)
+
+def desert_water_usage_rule_direct(tank_level, days_since_rain):
+    tank_level_set = np.asarray(fuzzifier.tank_fullness(tank_level))
+    rainfall_recency = fuzzifier.days_since_rainfall(days_since_rain)[0]
     rainfall_deficit = 1 - rainfall_recency
-    normal_usage = max(tank_level_set[1] + tank_level_set[0]*rainfall_deficit, 1)
-    conservative_usage = tank_level_set[0] - tank_level_set[0]*rainfall_recency
-    generous_usage = tank_level_set[2] - tank_level_set[0]*rainfall_recency
-    return [conservative_usage, normal_usage, generous_usage]
+    conservative_usage = tank_level_set[0] * (1 - rainfall_recency)
+    normal_usage = np.clip(tank_level_set[1] + tank_level_set[0] * rainfall_deficit, 0, 1)
+    generous_usage = tank_level_set[2]
+    return np.array([conservative_usage, normal_usage, generous_usage])
+
+space = np.linspace(0, 1, 101)
+plt.plot(space, general_watering_rule_direct(space)[0], label = "Необхідність поливу рослини")
+plt.title("Необхідність поливу рослини")
+plt.xlabel("Вологість грунту")
+plt.ylabel("Знач. функ. приналежності")
+plt.legend()
+plt.show()
+
+space = np.linspace(0, 10, 11)
+plt.plot(space, general_do_not_water_rule_direct(space)[0], label = "Не поливати рослину")
+plt.title("Не поливати рослину")
+plt.xlabel("Днів після зависокої вологості")
+plt.ylabel("Знач. функ. приналежності")
+plt.legend()
+plt.show()
+
+
+space = np.linspace(0, 1, 101)
+twarc = twarc_rule(space)
+
+plt.plot(space, twarc[0], label = "Використовувати водогін")
+plt.plot(space, twarc[1], label = "Використовувати дощову воду")
+plt.plot(space, twarc[2], label = "Зливати дощову воду в найсухішу рослину")
+plt.title("Не поливати рослину")
+plt.xlabel("Наповнення ємності")
+plt.ylabel("Знач. функ. приналежності")
+plt.legend()
+plt.show()
+
+for day in range(0, 11):
+    data = desert_water_usage_rule_direct(space, day)[0]
+    plt.plot(space, data, label = f"Дощ був {day} днів тому")
+plt.title("Економити воду")
+plt.xlabel("Наповнення ємності")
+plt.ylabel("Знач. функ. приналежності")
+plt.legend()
+plt.show()
+
+for day in range(0, 11):
+    data = desert_water_usage_rule_direct(space, day)[1]
+    plt.plot(space, data, label = f"Дощ був {day} днів тому")
+plt.title("Нормальне використання води")
+plt.xlabel("Наповнення ємності")
+plt.ylabel("Знач. функ. приналежності")
+plt.legend()
+plt.show()
+
+for day in range(0, 11):
+    data = desert_water_usage_rule_direct(space, day)[2]
+    plt.plot(space, data, label = f"Дощ був {day} днів тому")
+plt.title("Злив води в сухі рослини")
+plt.xlabel("Наповнення ємності")
+plt.ylabel("Знач. функ. приналежності")
+plt.legend()
+plt.show()
